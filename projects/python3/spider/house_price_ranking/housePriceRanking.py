f214fe9e69f250de38aaf7bc180e7360f48a34fd
richard <myPC@home.com>
2018-03-22 22:46:40 +0800

    update house price ranking -- well done
    Signed-off-by: richard <myPC@home.com>

--- projects/python3/spider/house_price_ranking/housePriceRanking.py
+++ projects/python3/spider/house_price_ranking/housePriceRanking.py
@@ -2,28 +2,48 @@
 import requests
 from lxml import etree
 
-headers = {
-    'User-Agent':'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.87 Safari/537.36'
-}
-url = 'http://esf.taizhou.fang.com/house-xm1818080456/'
-wbData = requests.get(url,headers=headers).text
-# print(wbData)
-tree = etree.HTML(wbData)
-# urls = tree.xpath('//*[@class="info rel floatr"]/p[1]/a/@href')
-# url = []
-# url_main = 'http://esf.taizhou.fang.com'
-# for i in urls:
-#     url.append(url_main + i)
+def gotWeb(url):
+    baseUrl = 'http://esf.taizhou.fang.com/'
+    wbData = requests.get(url).content.decode("gbk")
+    tree = etree.HTML(wbData)
+    websites = tree.xpath('//*[@id="list_D10_15"]/a/@href')
+    webs = []
+    for web in websites:
+        webs.append(baseUrl+web)
+    return(webs)
+
+data = []
+def gotData(url):
+    headers = {
+        'User-Agent':'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.87 Safari/537.36'
+    }
+    wbData = requests.get(url,headers=headers).content.decode("gbk")
+    tree = etree.HTML(wbData)
+    prices = tree.xpath('//*[@class="info rel floatr"]/div[3]/p[1]/span[1]/text()')
+    areas  = tree.xpath('//*[@class="info rel floatr"]/div[2]/p[1]/text()')
+    averages = tree.xpath('//*[@class="info rel floatr"]/div[@class="moreInfo"]/p[2]/text()[1]')
+    print(areas)
+    print(prices)
+    print(averages)
+    for area,price,average in zip(areas,prices,averages):
+        info = {
+            'area':area,
+            'price':price,
+            'average':average
+        }
+        data.append(info)
 
 
-prices = tree.xpath('//*[@class="info rel floatr"]/div[3]/p[1]/span[1]/text()')
-areas  = tree.xpath('//*[@class="info rel floatr"]/div[2]/p[1]/text()')
-averages = tree.xpath('//*[@class="info rel floatr"]/div[@class="moreInfo"]/p[2]/text()[1]')
+url = 'http://esf.taizhou.fang.com/house-xm1818080456/i32/'
+gotData(url)
+for i in gotWeb(url):
+    gotData(i)
 
-print(prices)
-print(areas)
-print(averages)
-print(len(averages))
+# print(areas)
+# print(prices)
+# print(averages)
+# print(len(averages))
+# print(data)
 
 # //*[@id="list_D03_01"]/dd/div[@class="moreInfo"]/p[2]
 
